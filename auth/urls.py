8e033d2b9e4dfaf274b10662cb3e0c9e7f77f35a
Manish-Kumar-Pandey <diwakarpandey767@gmail.com>
2021-08-24 12:14:27 +0530

    Change Existing File

--- auth/urls.py
+++ auth/urls.py
@@ -6,9 +6,6 @@ from django.conf.urls.static import static
 from django.views.static import serve
 from django.views.static import static
 from django.conf.urls import url
-
-
-
 urlpatterns = [
     path('', views.home, name = "home"),
     path('signup', views.signup, name = "signup"),
