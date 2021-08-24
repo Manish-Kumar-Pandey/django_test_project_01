be26ac08fe4a9ed107fd7aa3b935af8bed8b2551
Manish-Kumar-Pandey <diwakarpandey767@gmail.com>
2021-08-24 10:39:30 +0530

    First Commit -1

---
+++ auth/urls.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.static import serve
+from django.views.static import static
+from django.conf.urls import url
+
+
+
+urlpatterns = [
+    path('', views.home, name = "home"),
+    path('signup', views.signup, name = "signup"),
+    path('activate/<uidb64>/<token>', views.activate, name='activate'),
+    path('signin', views.signin, name = "signin"),
+    path('signout', views.signout, name = "signout"),
+
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root':settings.STATIC_ROOT}), 
+]
---
+++ auth/views.py
@@ -0,0 +1,57 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.shortcuts import redirect, render
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth import authenticate, login
+
+# Create your views here.
+def home(request):
+    return render(request,"auth/index.html")
+
+@csrf_exempt
+def signup(request):
+
+    if request.method == "POST":
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        psw = request.POST.get('psw')
+        psw2 = request.POST.get('psw2')
+        address = request.POST.get('address')
+
+
+        myuser = User.objects.create_user(username, email, psw)
+
+
+        myuser.save()
+        messages.success(request, "Your acount has been successfully created.")
+        return render(request,"auth/index.html")
+
+    return render(request,"auth/signup.html")
+
+
+
+@csrf_exempt
+def signin(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        psw = request.POST.get('psw')
+
+
+        user = authenticate(username=username, password = psw)
+
+        if user is not None:
+            login(request, user)
+            return render(request, "auth/Home-page.html")
+
+        else:
+            messages.error("Wrong Credentials")
+            return redirect('home')
+
+    return render(request,"auth/signin.html")
+
+def signout(request):
+    logout(request)
+    messages.success(request, "Logged Out Successfully!!")
+    return redirect('home')
